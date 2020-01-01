c79957737faddf74f3837996b7470ad39e9bd697
Andrea Francia <andrea@andreafrancia.it>
2020-01-01 19:06:40 +0100

    Add a fake Main.java and a stupid FirstTest.java

---
+++ src/test/java/kata/FirstTest.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+    @Test
+    public void something() {
+        assertEquals(1,1);
+    }
+}
